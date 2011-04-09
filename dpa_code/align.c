8e85b209d7dbba5bb2817fad7ac79b29588b5d0a
hunz <Zn000h@gmail.com>
2011-04-09 22:29:25 +0200

    better alignment code

--- dpa_code/align.c
+++ dpa_code/align.c
@@ -19,33 +19,44 @@ float lsqd(uint8_t *p1, uint8_t *p2, int len) {
 
 int main(int argc, char **argv) {
 	mf_t mf, mf2;
+	char buf[128];
 	int i, res, len, min_i = 0;
 	float min_diff = 0;
 
-	assert(argc>2);
-	res = map_file(&mf, argv[1], 0, 0);
-	assert(!res);
-	res = map_file(&mf2, argv[2], 0, 0);
+	assert(argc>1);
+	res = map_file(&mf, argv[1], 0, 0);	
 	assert(!res);
 
-	assert(mf.len == mf2.len);
 	len = mf.len;
 
 	assert(len >= 400000);
 
-	for(i=-10000;i<=10000;i++) {
-		float val = lsqd(mf.ptr + len/2, mf2.ptr + len/2 + i, 10000);
-		if(min_diff == 0.0) {
-			min_diff = val;
-			min_i = i;
+	while(fgets(buf,128,stdin)) {
+		int slen = strlen(buf);
+		buf[--slen]=0;
+
+		res = map_file(&mf2, buf, 0, 0);
+		assert(!res);
+
+		assert(mf2.len == len);
+
+		min_diff = -1.0;
+		for(i=-1000;i<=1000;i++) {
+			float val = lsqd(mf.ptr + 90000, mf2.ptr + 90000 + i, 1000);
+			if(min_diff == -1.0) {
+				min_diff = val;
+				min_i = i;
+			}
+			if(val < min_diff) {
+				min_diff = val;
+				min_i = i;
+			}
+			//printf("%d %f\n",i,val);
 		}
-		if(val < min_diff) {
-			min_diff = val;
-			min_i = i;
-		}
-		printf("%d %f\n",i,val);
+		printf("%s %d %f\n",buf,min_i,min_diff);
+
+		unmap_file(&mf2);
 	}
-	fprintf(stderr,"min diff %f at %d\n",min_diff,min_i);
 
 	return 0;
 }
